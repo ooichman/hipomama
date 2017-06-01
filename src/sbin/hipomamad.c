2e2ec33f509b503066b37228a5ed72636848c085
ooichman <two.oes@gmail.com>
2017-06-01 11:42:05 +0300

    initial commit

---
+++ src/sbin/hipomamad.c
@@ -0,0 +1,26 @@
+/*
+ * the hipomamad is an open source project for Enterprise central
+ * tasks scheduling for the Unix/Linux platform
+ * Copyright (C) yyyy  Oren Oichman
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Gnomovision version 1, Copyright (C) Oren Oichman
+ * Gnomovision comes with ABSOLUTELY NO WARRANTY; for details
+ * type `csv2* -h'.  This is free software, and you are welcome
+ * to redistribute it under certain conditions; type `hipomamctl -h' 
+ * for details.
+*/
+
